5089ec5a56bfeab18fa5ae3995dce609cbb8b2ef
James Tauber <jtauber@jtauber.com>
2000-09-18 02:08:13 +0000

    fully qualified rdf constants

--- redfoot-0.9/query.py
+++ redfoot-0.9/query.py
@@ -25,14 +25,14 @@ class QueryStore:
         return self.store.get(subject, predicate, object)
 
     def label(self, subject):
-        l = self.get(subject, LABEL, None)
+        l = self.get(subject, QueryStore.LABEL, None)
         if len(l) > 0:
             return l[0][2]     # TODO: currently only returns first label
         else:
             return subject
 
     def comment(self, subject):
-        c = self.get(subject, COMMENT, None)
+        c = self.get(subject, QueryStore.COMMENT, None)
         if len(c) > 0:
             return c[0][2]     # TODO: currently only returns first comment
         else:
@@ -40,19 +40,19 @@ class QueryStore:
 
     def getByType(self, type, predicate, object):
         l = []
-        for s in self.get(None, TYPE, type):
+        for s in self.get(None, QueryStore.TYPE, type):
             l.extend(self.get(s[0], predicate, object))
         return l
 
     def isOfType(self, resource, type):
-        for s in model.store.get(resource, TYPE, None):
+        for s in model.store.get(resource, QueryStore.TYPE, None):
             if s[2] == type:
                 return 1
         return 0
 
     def typeInh(self, t):
         l = []
-        for s in self.get(t, SUBCLASSOF, None):
+        for s in self.get(t, QueryStore.SUBCLASSOF, None):
             l.extend(self.typeInh(s[2]))
         return [t,l]
 
@@ -60,7 +60,7 @@ class QueryStore:
         set = {}
         set[type] = 1
 
-        for subclassStatement in self.get(type, SUBCLASSOF, None):
+        for subclassStatement in self.get(type, QueryStore.SUBCLASSOF, None):
             for item in self.transitiveSuperTypes(subclassStatement[2]):
                 set[item] = 1
 
@@ -70,7 +70,7 @@ class QueryStore:
         set = {}
         set[type] = 1
 
-        for subclassStatement in self.store.get(None, SUBCLASSOF, type):
+        for subclassStatement in self.store.get(None, QueryStore.SUBCLASSOF, type):
             for item in self.transitiveSubTypes(subclassStatement[0]):
                 set[item] = 1
 
