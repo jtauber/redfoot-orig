988ef0cba5d3942ab1905b665f76d1961a124e8e
Daniel Krech <eikeon@eikeon.com>
2000-09-18 15:11:25 +0000

    renamed store indicies to spv and pvs from localRDF and localByProperty

--- redfoot-0.9/store.py
+++ redfoot-0.9/store.py
@@ -2,28 +2,28 @@ class RDFStore:
 
     def __init__(self):
 	# indexed by [subject][property][value]
-        self.localRDF = {}
+        self.spv = {}
 
         # indexed by [property][value][subject]
-        self.localByProperty = {}
+        self.pvs = {}
 
     def add(self, subject, property, value):
-        if not subject in self.localRDF.keys():
-            self.localRDF[subject] = {}
+        if not subject in self.spv.keys():
+            self.spv[subject] = {}
 
-        if not property in self.localRDF[subject].keys():
-            self.localRDF[subject][property] = {}
+        if not property in self.spv[subject].keys():
+            self.spv[subject][property] = {}
 
-        self.localRDF[subject][property][value] = 1
+        self.spv[subject][property][value] = 1
 
         # add to byProperty
-        if not property in self.localByProperty.keys():
-            self.localByProperty[property] = {}
+        if not property in self.pvs.keys():
+            self.pvs[property] = {}
 
-        if not value in self.localByProperty[property].keys():
-            self.localByProperty[property][value] = {}
+        if not value in self.pvs[property].keys():
+            self.pvs[property][value] = {}
 
-        self.localByProperty[property][value][subject] = 1
+        self.pvs[property][value][subject] = 1
 
     def put(self, subject, property, value):
         self.remove(subject, property, value)
@@ -33,32 +33,32 @@ class RDFStore:
         list = []
 
         if subject!=None:
-            for s in self.localRDF.keys():
+            for s in self.spv.keys():
                 if subject == None or subject == s:
-                    for p in self.localRDF[s].keys():
+                    for p in self.spv[s].keys():
                         if property == None or property == p:
-                            for v in self.localRDF[s][p].keys():
+                            for v in self.spv[s][p].keys():
                                 if value == None or value == v:
                                     list.append((s, p, v))
         else:
-            for p in self.localByProperty.keys():
+            for p in self.pvs.keys():
                 if property == None or property == p:
-                    for v in self.localByProperty[p].keys():
+                    for v in self.pvs[p].keys():
                         if value == None or value == v:
-                            for s in self.localByProperty[p][v].keys():
+                            for s in self.pvs[p][v].keys():
                                 if subject == None or subject == s:
                                     list.append((s, p, v))
             
 	return list
 
     def remove(self, subject, property, value):
-        if subject in self.localRDF.keys() and property in self.localRDF[subject].keys():
-            if self.localRDF[subject][property].has_key(value):
-                del self.localRDF[subject][property][value]
+        if subject in self.spv.keys() and property in self.spv[subject].keys():
+            if self.spv[subject][property].has_key(value):
+                del self.spv[subject][property][value]
 
-        if property in self.localByProperty.keys() and value in self.localByProperty[property].keys():
-            if self.localByProperty[property][value].has_key(subject):
-                del self.localByProperty[property][value][subject]
+        if property in self.pvs.keys() and value in self.pvs[property].keys():
+            if self.pvs[property][value].has_key(subject):
+                del self.pvs[property][value][subject]
 
     def removeAll(self, subject):
         list = self.get(subject, None, None)
