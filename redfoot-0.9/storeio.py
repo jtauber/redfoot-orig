a355d55b62b7e0e89c1bbbc956f4e4ecf6047d22
Daniel Krech <eikeon@eikeon.com>
2000-09-19 03:44:56 +0000

    added .cvsignore to ignore python cache files

--- redfoot-0.9/storeio.py
+++ redfoot-0.9/storeio.py
@@ -1,4 +1,4 @@
-from redfoot.store import *
+#from redfoot.store import *
 from redfoot.parser import *
 
 class StoreIO:
@@ -9,9 +9,14 @@ class StoreIO:
     def getStore(self):
         return self.store
 
-    def load(self, location, URI):
+    def load(self, location, URI=None):
         self.location = location
-        self.URI = URI
+        if URI==None:
+            # default to location
+            self.URI = self.location
+        else:
+            self.URI = URI
+
         rdfParser = RDFParser()
         rdfParser.setAdder(self.store.add)
 
@@ -21,4 +26,16 @@ class StoreIO:
         self.saveAs(self.location, self.URI)
 
     def saveAs(self, location, URI):
-        pass #TODO
+        
+        s = Serializer()
+        s.setBase(self.URI)
+
+         
+        s.registerProperty("foo#author")
+        s.start()
+        
+        s.subjectStart("http://jtauber.com")
+        s.property("foo#author","^James Tauber")
+        s.subjectEnd()
+        
+        s.end()
