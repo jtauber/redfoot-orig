a4c1780fb89b9703d7baad2fb9c60e9e54070102
James Tauber <jtauber@jtauber.com>
2000-09-19 03:21:11 +0000

    added more test cases for querying

--- redfoot-0.9/tests/query-tests.py
+++ redfoot-0.9/tests/query-tests.py
@@ -15,7 +15,27 @@ qstore = QueryStore()
 qstore.setStore(store)
 
 print qstore.get(qstore.CLASS, qstore.TYPE, None)
-
 print qstore.label(qstore.CLASS)
 print qstore.comment(qstore.CLASS)
 
+store.add("vehicle", qstore.TYPE, qstore.CLASS)
+store.add("vehicle", qstore.SUBCLASSOF, qstore.RESOURCE)
+store.add("car", qstore.TYPE, qstore.CLASS)
+store.add("car", qstore.SUBCLASSOF, "vehicle")
+store.add("my-car", qstore.TYPE, "car")
+store.add("my-car", qstore.LABEL, "^My Car")
+
+print qstore.getByType("car", None, None)
+print qstore.getByType("car", qstore.LABEL, "^My Car")
+
+print qstore.isOfType("my-car", "car")
+print qstore.isOfType("my-car", "vehicle")
+print qstore.isOfType("my-car", "giraffe")
+
+store.add("car", qstore.SUBCLASSOF, "fast-things")
+
+print qstore.typeInh("car")
+
+print qstore.transitiveSuperTypes("car")
+
+print qstore.transitiveSubTypes("vehicle")
