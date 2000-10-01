6fc842fa9c663421763d206aa3cc05b45e4e72c9
Daniel Krech <eikeon@eikeon.com>
2000-10-01 07:28:17 +0000

    now takes port number to listen on, filename of rdf file to load, and URI to load the rdf as

--- redfoot-0.9/run.bat
+++ redfoot-0.9/run.bat
@@ -1,11 +1,12 @@
 @echo off
+rem python %1 -- use this if/when we need to run more than one thing and when we can kick out a usage message... for now... TODO: un hard code the arguments
+
 setlocal
 set PYTHONPATH=../
 @echo on
 
-rem python %1 -- use this if/when we need to run more than one thing and when we can kick out a usage message... for now...
 
-python server.py
+python server.py 8000 tests/services.rdf http://www.bowstreet.com/2000/08/20
 
 
 @echo off
