c83a08b998d3834e9b294aac793a599d40b032f1
James Tauber <jtauber@jtauber.com>
2000-10-08 22:28:42 +0000

    fixed PeerEditor so Class List menu item goes to classList rather than default page

--- redfoot-0.9/editor.py
+++ redfoot-0.9/editor.py
@@ -308,7 +308,7 @@ class Editor(Viewer):
 class PeerEditor(Editor):
     def menuBar(self):
         self.writer.write("""
-            <P CLASS="MENUBAR"><A HREF=".">Class List</A>
+            <P CLASS="MENUBAR"><A HREF="classList">Class List</A>
              | <A HREF="subclass">Full Subclass View</A>
              | <A HREF="subclassNR">Collapsed Subclass View</A>
              | <A HREF="RDF">Download RDF</A>
