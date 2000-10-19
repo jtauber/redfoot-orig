660ab85a3f5ebfc09d96e9e829b1d13b54b70b3c
Daniel Krech <eikeon@eikeon.com>
2000-10-19 16:10:04 +0000

    changed run.bat to run the sample

--- redfoot-0.9/run.bat
+++ redfoot-0.9/run.bat
@@ -1,12 +1,12 @@
 @echo off
-rem python %1 -- use this if/when we need to run more than one thing and when we can kick out a usage message... for now... TODO: un hard code the arguments
+
+rem Runs the redfoot server with the SampleUI 
 
 setlocal
 set PYTHONPATH=../
 @echo on
 
-
-python server.py -p 8000 -l ./example.rdf -u http://www.bowstreet.com/2000/08/20 -i PeerEditor
+python server.py -P /2000/10/redfoot -i SampleUI -l sampleui.rdf
 
 
 @echo off
