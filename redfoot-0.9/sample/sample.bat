6fe617139ffab066f6948df8a970910348c6c211
Daniel Krech <eikeon@eikeon.com>
2000-10-26 23:43:17 +0000

    put sample.bat back as it was

--- redfoot-0.9/sample/sample.bat
+++ redfoot-0.9/sample/sample.bat
@@ -4,8 +4,7 @@ setlocal
 set PYTHONPATH=../lib/
 @echo on
 
-rem python ./sample.py -P /2000/10/redfoot/ -l sample.rdf
-python ./sample.py -P / -l sample.rdf
+python ./sample.py -P /2000/10/redfoot/ -l sample.rdf
 
 @echo off
 endlocal
