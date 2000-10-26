8c7b72929c136098bd7c48ca4513dec6bfbe31cb
James Tauber <jtauber@jtauber.com>
2000-10-26 02:14:54 +0000

    fixed copy/paste error

--- redfoot-0.9/redfoot.sh
+++ redfoot-0.9/redfoot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-PYTHONPATH=../lib/:$PYTHONPATH; export PYTHONPATH
+PYTHONPATH=./lib/:$PYTHONPATH; export PYTHONPATH
 
-python ./sample.py -P /2000/10/redfoot -l sample.rdf
+python ./redfoot.py
