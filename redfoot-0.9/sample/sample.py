8ad95c27df9ef9185b119ba3090df1d8a4e2813b
Daniel Krech <eikeon@eikeon.com>
2000-11-04 01:48:43 +0000

    moved sample authentication from editor.py to sample.py

--- redfoot-0.9/sample/sample.py
+++ redfoot-0.9/sample/sample.py
@@ -9,6 +9,8 @@ class SampleUI(BaseUI):
         path_info = request.getPathInfo()
 
         if self.path_match(path_info):
+            if not self.authenticated(request, response):
+                return
             self.call_editor(request, response)
         elif path_info=="/":
             self.main(response)
@@ -48,12 +50,52 @@ class SampleUI(BaseUI):
         """)
 
 
+    def authenticated(self, request, response):
+        parameters = request.getParameters()
+        session = request.getSession()
+        if hasattr(session, 'username'):
+            return 1
+        elif parameters['username']!="" and \
+           parameters['password']!="" and parameters['password']=="redfoot":
+                session.username = parameters['username']
+                return 1
+        else:
+                response.write("""
+<HTML>
+  <HEAD>
+    <TITLE>Username</TITLE>
+  </HEAD>
+  <H1>Username</H1>
+  <FORM method="POST">
+  <TABLE>
+    <TR>
+      <TD>Username:</TD>
+      <TD><INPUT name="username" type="text"> (Hint: any username will work)</TD>
+    </TR>
+    <TR>
+      <TD>Password:</TD>
+      <TD><INPUT name="password" type="text"> (Hint: refoot)</TD>
+      </TD>
+    </TR>
+    <TR>
+      <TD colspan=2"><INPUT value="Login" type="submit"></TD>
+    </TR>
+  </FORM>
+</HTML>
+""")
+                return 0
+
+        raise "TODO: exception indicating we should never fall though to here"
+            
 if __name__ == '__main__':
     import sys
     from redfoot.server import runServer
     runServer(sys.argv[1:], SampleUI)
 
 # $Log$
+# Revision 1.5  2000/11/03 23:04:08  eikeon
+# Added support for cookies and sessions; prefixed a number of methods and variables with _ to indicate they are private; changed a number of methods to mixed case for consistency; added a setHeader method on response -- headers where hardcoded before; replaced writer with response as writer predates and is redundant with repsonse; Added authentication to editor
+#
 # Revision 1.4  2000/11/02 21:48:27  eikeon
 # removed old log messages
 #
