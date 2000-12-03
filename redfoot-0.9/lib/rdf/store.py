9efba82b940f8afd349e711659735d389c6f6f04
James Tauber <jtauber@jtauber.com>
2000-12-03 20:17:40 +0000

    changed property/value to predicate/object

--- redfoot-0.9/lib/rdf/store.py
+++ redfoot-0.9/lib/rdf/store.py
@@ -3,98 +3,102 @@
 class TripleStore:
 
     def __init__(self):
-	# indexed by [subject][property][value]
-        self.spv = {}
+	# indexed by [subject][predicate][object]
+        self.spo = {}
 
-        # indexed by [property][value][subject]
-        self.pvs = {}
+        # indexed by [predicate][object][subject]
+        self.pos = {}
 
-    def add(self, subject, property, value):
-        if not self.spv.has_key(subject):
-            self.spv[subject] = {}
+    def add(self, subject, predicate, object):
+        # spo
+        if not self.spo.has_key(subject):
+            self.spo[subject] = {}
 
-        if not self.spv[subject].has_key(property):
-            self.spv[subject][property] = {}
+        if not self.spo[subject].has_key(predicate):
+            self.spo[subject][predicate] = {}
 
-        self.spv[subject][property][value] = 1
+        self.spo[subject][predicate][object] = 1
 
-        # add to byProperty
-        if not self.pvs.has_key(property):
-            self.pvs[property] = {}
+        # pos
+        if not self.pos.has_key(predicate):
+            self.pos[predicate] = {}
 
-        if not self.pvs[property].has_key(value):
-            self.pvs[property][value] = {}
+        if not self.pos[predicate].has_key(object):
+            self.pos[predicate][object] = {}
 
-        self.pvs[property][value][subject] = 1
+        self.pos[predicate][object][subject] = 1
 
-    def put(self, subject, property, value):
-        self.remove(subject, property, value)
-        self.add(subject, property, value)
+    def put(self, subject, predicate, object):
+        self.remove(subject, predicate, object)
+        self.add(subject, predicate, object)
 
-    def get(self, subject=None, property=None, value=None):
+    def get(self, subject=None, predicate=None, object=None):
         class Visitor:
             def __init__(self):
                 self.list = []
 
-            def callback(self, subject, property, value):
-                self.list.append((subject, property, value))
+            def callback(self, subject, predicate, object):
+                self.list.append((subject, predicate, object))
 
         visitor = Visitor()
-        self.visit(visitor.callback, subject, property, value)
+        self.visit(visitor.callback, subject, predicate, object)
 
 	return visitor.list
 
-    def remove(self, subject=None, property=None, value=None):
+    def remove(self, subject=None, predicate=None, object=None):
         class Visitor:
             def __init__(self, store):
                 self.store = store
 
-            def callback(self, subject, property, value):
-                del self.store.spv[subject][property][value]
-                del self.store.pvs[property][value][subject]
+            def callback(self, subject, predicate, object):
+                del self.store.spo[subject][predicate][object]
+                del self.store.pos[predicate][object][subject]
 
         visitor = Visitor(self)
-        self.visit(visitor.callback, subject, property, value)
+        self.visit(visitor.callback, subject, predicate, object)
 
-    def visit(self, callback, subject=None, property=None, value=None):
+    def visit(self, callback, subject=None, predicate=None, object=None):
         if subject!=None:
-            if self.spv.has_key(subject):
-                if property!=None:
-                    if self.spv[subject].has_key(property):
-                        if value!=None:
-                            if self.spv[subject][property].has_key(value):
-                                callback(subject, property, value)
+            if self.spo.has_key(subject):
+                if predicate!=None:
+                    if self.spo[subject].has_key(predicate):
+                        if object!=None:
+                            if self.spo[subject][predicate].has_key(object):
+                                callback(subject, predicate, object)
                         else:
-                            for v in self.spv[subject][property].keys():
-                                callback(subject, property, v)
+                            for o in self.spo[subject][predicate].keys():
+                                callback(subject, predicate, o)
                 else:
-                    for p in self.spv[subject].keys():
-                        self.visit(callback, subject, p, value) # recurse for now
+                    for p in self.spo[subject].keys():
+                        self.visit(callback, subject, p, object) # recurse for now
         else:
-            if property!=None:
-                if self.pvs.has_key(property):
-                    if value!=None:
-                        if self.pvs[property].has_key(value):
-                            for s in self.pvs[property][value].keys():
-                                callback(s, property, value)
+            if predicate!=None:
+                if self.pos.has_key(predicate):
+                    if object!=None:
+                        if self.pos[predicate].has_key(object):
+                            for s in self.pos[predicate][object].keys():
+                                callback(s, predicate, object)
                     else:
-                        for v in self.pvs[property].keys():
-                            for s in self.pvs[property][v].keys():
-                                callback(s, property, v)
+                        for o in self.pos[predicate].keys():
+                            for s in self.pos[predicate][o].keys():
+                                callback(s, predicate, o)
             else:
-                if value!=None:
-                    for p in self.pvs.keys():
-                        if self.pvs[p].has_key(value):
-                            for s in self.pvs[p][value]:
-                                callback(s, p, value)
+                if object!=None:
+                    for p in self.pos.keys():
+                        if self.pos[p].has_key(object):
+                            for s in self.pos[p][object]:
+                                callback(s, p, object)
                 else:
-                    for s in self.spv.keys():
-                        for p in self.spv[s].keys():
-                            for v in self.spv[s][p].keys():
-                                callback(s, p, v)
+                    for s in self.spo.keys():
+                        for p in self.spo[s].keys():
+                            for o in self.spo[s][p].keys():
+                                callback(s, p, o)
                     
 
 #~ $Log$
+#~ Revision 4.0  2000/11/06 15:57:33  eikeon
+#~ VERSION 4.0
+#~
 #~ Revision 3.1  2000/11/02 21:48:27  eikeon
 #~ removed old log messages
 #~
