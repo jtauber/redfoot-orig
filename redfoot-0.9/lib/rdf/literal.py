db37e49cb3ebe9683b5c3bdc05eaac9faf01840b
James Tauber <jtauber@jtauber.com>
2000-12-04 02:38:12 +0000

    added un_literal

--- redfoot-0.9/lib/rdf/literal.py
+++ redfoot-0.9/lib/rdf/literal.py
@@ -6,4 +6,10 @@ def literal(str):
 def is_literal(str):
     return str[0]=="^"
 
+def un_literal(str):
+    return str[1:]
+
 # $Log$
+# Revision 1.1  2000/12/03 22:23:13  jtauber
+# created literal.py for literal handling
+#
