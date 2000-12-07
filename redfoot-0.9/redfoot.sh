8fd5b9d9d9605d0e9583323d674a111d0e8cdec6
Daniel Krech <eikeon@eikeon.com>
2000-12-07 19:02:40 +0000

    moved to apps

--- redfoot-0.9/redfoot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export PYTHONPATH=./lib
-
-python ./lib/redfoot/server.py $*
-
--- redfoot-0.9/sample/sample.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export PYTHONPATH=../lib
-
-python ./sample.py -P /2000/10/redfoot -l sample.rdf
