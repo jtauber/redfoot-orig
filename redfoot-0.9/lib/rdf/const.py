6f14e89f53a29c98e05c4c29cc7daf30bf121a9b
James Tauber <jtauber@jtauber.com>
2000-12-07 19:45:46 +0000

    organized constants by syntax versus schema

--- redfoot-0.9/lib/rdf/const.py
+++ redfoot-0.9/lib/rdf/const.py
@@ -1,11 +1,17 @@
 # $Header$
 
+# SYNTAX
 RDFNS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-RDFSNS = "http://www.w3.org/2000/01/rdf-schema#"
-
 TYPE = RDFNS+"type"
-CLASS = RDFSNS+"Class"
 PROPERTY = RDFNS+"Property"
+STATEMENT = RDFNS+"Statement"
+SUBJECT = RDFNS+"subject"
+PREDICATE = RDFNS+"predicate"
+OBJECT = RDFNS+"object"
+
+# SCHEMA
+RDFSNS = "http://www.w3.org/2000/01/rdf-schema#"
+CLASS = RDFSNS+"Class"
 RESOURCE = RDFSNS+"Resource"
 SUBCLASSOF = RDFSNS+"subClassOf"
 LABEL = RDFSNS+"label"
@@ -13,9 +19,8 @@ COMMENT = RDFSNS+"comment"
 RANGE = RDFSNS+"range"
 DOMAIN = RDFSNS+"domain"
 LITERAL = RDFSNS+"Literal"
-STATEMENT = RDFNS+"Statement"
-SUBJECT = RDFNS+"subject"
-PREDICATE = RDFNS+"predicate"
-OBJECT = RDFNS+"object"
 
 # $Log$
+# Revision 4.1  2000/12/05 22:09:35  jtauber
+# moved constants to new file
+#
