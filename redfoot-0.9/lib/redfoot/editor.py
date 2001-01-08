8c555455fa3f389a5ab3d1986e780fcbe937be19
Daniel Krech <eikeon@eikeon.com>
2001-01-08 21:16:37 +0000

    fixed required property functionality just added to edit

--- redfoot-0.9/lib/redfoot/editor.py
+++ redfoot-0.9/lib/redfoot/editor.py
@@ -88,21 +88,14 @@ class Editor(Viewer):
         else:
             self.response.write("""<TR><TD>Resource not known of directly</TD></TR>""")
 
-        types = {}
-        triples = self.storeNode.get(subject, TYPE, None)
-        for triple in triples:
-            types[type] = 1
-            for t in self.storeNode.getTransitiveSuperTypes(triple[2]):
-                types[t] = 1
-            
-        for type in types.keys():
-            def possibleProperty(s, p, o, self=self, subject=subject):   
-                property = s   
-                if len(self.storeNode.get(property, self.REQUIREDPROPERTY, "http://redfoot.sourceforge.net/2000/10/06/builtin#YES"))>0 and len(self.storeNode.get(subject, property, None))==0:   
-                    self.editProperty(property, "", 0)   
-    
-            self.storeNode.visitPossibleProperties(possibleProperty, type ) 
-            
+        properties = {}
+        def possibleProperty(s, p, o, properties=properties):
+            properties[s] = 1
+        self.storeNode.visitPossiblePropertiesForSubject(possibleProperty, subject)
+        
+        for property in properties.keys():
+            if len(self.storeNode.get(property, self.REQUIREDPROPERTY, "http://redfoot.sourceforge.net/2000/10/06/builtin#YES"))>0 and len(self.storeNode.get(subject, property, None))==0:   
+                self.editProperty(property, "", 0)   
     
         self.response.write("""
           <TR>
@@ -351,6 +344,9 @@ class PeerEditor(Editor):
 
 
 #~ $Log$
+#~ Revision 5.18  2001/01/08 20:38:32  eikeon
+#~ edit now adds required properties if they are not already present
+#~
 #~ Revision 5.17  2000/12/28 03:00:55  jtauber
 #~ added 'copy' button to edit page
 #~
