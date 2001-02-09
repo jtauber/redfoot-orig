e9206beeeb912d3a4377869f2115d69d973a3760
Daniel Krech <eikeon@eikeon.com>
2001-02-09 21:46:57 +0000

    updated test cases

--- redfoot-0.9/lib/rdf/tests/query-tests.py
+++ redfoot-0.9/lib/rdf/tests/query-tests.py
@@ -1,58 +1,99 @@
 import pyexpat
 
-from redfoot.store import *
-from redfoot.storeio import *
-from redfoot.query import *
+from rdf.store import *
+from rdf.storeio import *
+from rdf.query import *
 
-store = TripleStore()
+from rdf.const import *
 
-storeIO = StoreIO()
-storeIO.setStore(store)
+from redfoot.rednode import RedNode 
 
-storeIO.load("rdfSyntax.rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns")
-storeIO.load("rdfSchema.rdf", "http://www.w3.org/2000/01/rdf-schema")
 
-qstore = QueryStore()
-qstore.setStore(store)
+store = RedNode()
 
-print qstore.get(qstore.CLASS, qstore.TYPE, None)
-print qstore.label(qstore.CLASS)
-print qstore.comment(qstore.CLASS)
+print store.get(CLASS, TYPE, None)
+print store.label(CLASS)
+print store.comment(CLASS)
 
-store.add("vehicle", qstore.TYPE, qstore.CLASS)
-store.add("vehicle", qstore.SUBCLASSOF, qstore.RESOURCE)
-store.add("car", qstore.TYPE, qstore.CLASS)
-store.add("car", qstore.SUBCLASSOF, "vehicle")
-store.add("my-car", qstore.TYPE, "car")
-store.add("my-car", qstore.LABEL, "^My Car")
+store.local.add("vehicle", TYPE, CLASS)
+store.local.add("vehicle", SUBCLASSOF, RESOURCE)
+store.local.add("car", TYPE, CLASS)
+store.local.add("car", SUBCLASSOF, "vehicle")
+store.local.add("two-door-car", TYPE, CLASS)
+store.local.add("two-door-car", SUBCLASSOF, "car")
+store.local.add("my-car", TYPE, "car")
+store.local.add("my-car", LABEL, "^My Car")
 
-print qstore.getByType("car", None, None)
-print qstore.getByType("car", qstore.LABEL, "^My Car")
+store.local.add("color", TYPE, PROPERTY)
+store.local.add("color", DOMAIN, "car")
+store.local.add("color", RANGE, LITERAL)
 
-print qstore.isOfType("my-car", "car")
-print qstore.isOfType("my-car", "vehicle")
-print qstore.isOfType("my-car", "giraffe")
+store.local.add("cooler_than", TYPE, PROPERTY)
+store.local.add("cooler_than", DOMAIN, "car")
+#store.local.add("cooler_than", RANGE, "car")
+store.local.add("cooler_than", RANGE, "vehicle")
 
-store.add("car", qstore.SUBCLASSOF, "fast-things")
 
-print qstore.typeInh("car")
 
-print qstore.transitiveSuperTypes("car")
+print store.getByType("car", None, None)
+print store.getByType("car", LABEL, "^My Car")
 
-print qstore.transitiveSubTypes("vehicle")
+print store.isOfType("my-car", "car")
+print store.isOfType("my-car", "vehicle")
+print store.isOfType("my-car", "giraffe")
 
-def cDisplay(c,indent=0):
-    print indent*"  ","-", c
+store.local.add("car", SUBCLASSOF, "fast-things")
 
-def rDisplay(r,indent=0):
-    print indent*"  ","  *",r
+#print store.typeInh("car")
 
-print qstore.resourcesByClassV(cDisplay, rDisplay)
+#print store.transitiveSuperTypes("car")
 
-print "root classes: ", qstore.rootClasses()
+#print store.transitiveSubTypes("vehicle")
+class StatementSetBuilder:
+    def __init__(self):
+        self.set = {}
 
-print 79*"-"
-print qstore.subClassV(qstore.RESOURCE, cDisplay, rDisplay)
+    def visit(self, s, p, o):
+        self.set[(s, p, o)] = 1
 
-print 79*"-"
-print qstore.getPossibleValues(qstore.RANGE)
+    def flush(self):
+        pass
+    
+visitor = StatementSetBuilder()
+store.visitTransitiveSubTypes(visitor, "vehicle")
+print "Transitive Sub Types of vehicle: %s" % visitor.set
+print "Transitive Sub Types of vehicle: %s" % store.getTransitiveSubTypes("vehicle")
+print "Transitive super Types of car: %s" % store.getTransitiveSuperTypes("two-door-car")
+
+visitor = StatementSetBuilder()
+store.visitPossibleValues(visitor.visit, "cooler_than")
+print "Possible values for property cooler_than: %s" % visitor.set
+
+visitor = StatementSetBuilder()
+store.visitPossiblePropertiesForSubject(visitor.visit, "my-car")
+print "Possible properties for my_car: %s" % visitor.set
+
+
+
+#def cDisplay(c,indent=0):
+#    print indent*"  ","-", c
+
+#def rDisplay(r,indent=0):
+#    print indent*"  ","  *",r
+
+#print store.resourcesByClassV(cDisplay, rDisplay)
+
+#print "root classes: ", store.rootClasses()
+
+#print 79*"-"
+#print store.subClassV(RESOURCE, cDisplay, rDisplay)
+
+#print 79*"-"
+#print store.getPossibleValues(RANGE)
+
+#          def pv(s, p, o, response=response):
+#              response.write("""
+#              <li>%s</li>           
+#          """ % s)
+#          self.storeNode.visitPossibleValues(pv, "http://xteam.hq.bowstreet.com/2000/10/#subject")
+
