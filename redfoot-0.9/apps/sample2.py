675c7a6f8bd539de8b27c4c2715c5e57401f9ad1
James Tauber <jtauber@jtauber.com>
2001-04-17 03:15:03 +0000

    changing sample2 and rss module to handle move of rss-related schemas

--- redfoot-0.9/apps/sample2.py
+++ redfoot-0.9/apps/sample2.py
@@ -15,10 +15,6 @@ if __name__ == "__main__":
 
     node = RedNode()
     node.local.load("sample2.rdf", uri )
-    node.connectTo("rssSchema.rdf", None)
-    node.connectTo("dces.xml", None)    
-    #node.connectTo("http://xmlhack.com/rss10.php", "http://xmlhack.com/rss10.php")
-    #node.connectTo("xmlhack.rdf", "xmlhack.rdf")        
 
     server.run_redpage("sample2.xml", node)
 
