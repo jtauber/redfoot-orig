cde22d4c71ae2937a8f0accaec9eee77011dafcc
James Tauber <jtauber@jtauber.com>
2001-09-08 04:53:30 +0000

    Moved components to new CVS module

---
+++ components/redmodules/static.py
@@ -0,0 +1,25 @@
+from redfoot.module import Module
+import os
+
+class Static(Module):
+
+    def static(self):
+        # Content-type, size, etc
+        self.serve_file(self.app.request.get_path_info()[1:])
+
+    def __getattr__(self, name):
+        path_info = self.app.request.get_path_info()        
+        if name == path_info:
+            file = path_info[1:]
+            if os.path.exists(file) and os.path.isfile(file):
+                return self.static
+        raise AttributeError, name
+            
+    def serve_file(self, file, type='text/html'):
+        self.app.response.set_header("Content-Type", type)
+        import shutil
+        source = open(file, 'rb')
+        destination = self.app.response
+        shutil.copyfileobj(source, destination)
+        source.close()
+
