809518da49443f5fbfbe12355cf822144583dc35
Daniel Krech <eikeon@eikeon.com>
2001-09-22 05:58:52 +0000

    Changed to match new daemon interface

--- components/run.py
+++ components/run.py
@@ -1,11 +1,10 @@
-
 import sys
-sys.path.extend(("../redfoot-1.1/core", ))
+sys.path.extend(("../redfoot/core", ))
 
 from redfoot.server.http.daemon import command_line
 from redcode.handlers import RedcodeRootHandler
 
 daemon = command_line()
-daemon.handler_class = RedcodeRootHandler
+daemon.handlers = {'xml': RedcodeRootHandler}
 daemon.run()        
     
