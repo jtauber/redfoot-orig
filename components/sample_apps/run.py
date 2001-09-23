ac125669f16d9238bde1565f1d81f00318bad59f
Daniel Krech <eikeon@eikeon.com>
2001-09-23 21:03:42 +0000

    run now checks to see if redfoot is on the PYTHONPATH before attempting to add it via a relative path

--- components/sample_apps/run.py
+++ components/sample_apps/run.py
@@ -1,6 +1,16 @@
-import sys
-sys.path.extend(("../../redfoot-1.1/core", ))
-sys.path.extend(("../", ))
+
+try:
+    from redfoot.version import VERSION
+except ImportError:
+    # Try to add redfoot to the path via relative path
+    import sys
+    sys.path.extend(("../../redfoot-1.1.0/core", ))
+    sys.path.extend(("../", ))
+    try:
+        from redfoot.version import VERSION
+        print "Found Redfoot-%s" % VERSION        
+    except:
+        print "Could not find core Redfoot library"
 
 from redfoot.server.http.daemon import command_line
 from redcode.handlers import RedcodeRootHandler
