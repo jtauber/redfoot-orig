9c6c8d8cf9cd164bc32cd955a16ac2e4bbc3e7dc
Daniel Krech <eikeon@eikeon.com>
2001-09-24 02:24:03 +0000

    added ../ to path independant of adding redfoot to the path

--- components/sample_apps/run.py
+++ components/sample_apps/run.py
@@ -1,17 +1,19 @@
+import sys
 
 try:
     from redfoot.version import VERSION
 except ImportError:
     # Try to add redfoot to the path via relative path
-    import sys
     sys.path.extend(("../../redfoot-1.1.0/core", ))
-    sys.path.extend(("../", ))
     try:
         from redfoot.version import VERSION
         print "Found Redfoot-%s" % VERSION        
     except:
         print "Could not find core Redfoot library"
 
+# add components directory to PYTHONPATH
+sys.path.extend(("../", ))
+    
 from redfoot.server.http.daemon import command_line
 from redcode.handlers import RedcodeRootHandler
 
