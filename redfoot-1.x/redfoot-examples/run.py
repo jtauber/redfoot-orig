18fbff9d0adbb4dadae7f85e1ee9e8068acd9460
Daniel Krech <eikeon@eikeon.com>
2002-04-11 04:39:30 +0000

    removed reference to file that is not checked in

--- redfoot-1.x/redfoot-examples/run.py
+++ redfoot-1.x/redfoot-examples/run.py
@@ -1,12 +1,12 @@
-import sys
-sys.path.extend(("../redfoot-core", "../redfoot-components"))
+#import sys
+#sys.path.extend(("../redfoot-core", "../redfoot-components"))
 
 # Needed to be able to import redcode Modules
 from redfoot.redcode import importer
 importer.install()
 
 # Import Modules you wish to have available
-import hello_world, generic, redfoot_net #, grid, foaf, pop, rss, task
+import hello_world, generic, #redfoot_net, grid, foaf, pop, rss, task
 
 # 
 from redfoot.rednode import RedNode
