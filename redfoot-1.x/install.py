d67cc68ef379ccc38c0c3cdd32efd9c463acdcbf
Daniel Krech <eikeon@eikeon.com>
2002-04-12 03:28:32 +0000

    fixed current directory problem... not sure why it was originally working for me

--- redfoot-1.x/install.py
+++ redfoot-1.x/install.py
@@ -1,17 +1,22 @@
 # Convience script for installing both medusa and redfoot at the same
 # time
-
 import sys, os
 
+dot = os.path.abspath(os.curdir)
 python = sys.executable
 args = " ".join(sys.argv[1:])
 
+def run_setup(dir_name):
+    os.chdir(dir_name)
+    os.system("%s setup.py install %s" % (python, args))
+    os.chdir(dot)
+
 # TODO: first check to see if they already have a version of medusa
 # install. If so, let them install individually on their own.
-os.system("%s medusa/setup.py install %s" % (python, args))
+run_setup("medusa")
 
-os.system("%s redfoot-core/setup.py install %s" % (python, args))
-os.system("%s redfoot-components/setup.py install %s" % (python, args))
+run_setup("redfoot-core")
+run_setup("redfoot-components")
 
 # TODO: would it make sense for this to be a setup.py script as well
 # to copy redfoot-doc and redfoot-examples somewhere... etc?
