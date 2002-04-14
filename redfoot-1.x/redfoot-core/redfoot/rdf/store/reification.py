fa1e47d62046f7d8d7f59643be42a01d869f7749
Daniel Krech <eikeon@eikeon.com>
2002-04-14 19:44:05 +0000

    added __init__ that calls super so that in practice it can be mixed in

--- redfoot-1.x/redfoot-core/redfoot/rdf/store/reification.py
+++ redfoot-1.x/redfoot-core/redfoot/rdf/store/reification.py
@@ -1,7 +1,10 @@
 
-class ReificationStore:
+class ReificationStore(object):
     """Mixin to enable reification."""
     
+    def __init__(self):
+        super(ReificationStore, self).__init__()
+
     def reify(self, statement_uri, subject, predicate, object):
         self.add(statement_uri, TYPE, STATEMENT)
         self.add(statement_uri, SUBJECT, subject)
