4ee3537bd9dd4f54f4f8a4c3098bb18e4b906e8d
Daniel Krech <eikeon@eikeon.com>
2002-04-19 23:44:41 +0000

    minor bug fix; added import dev_hack;

--- redfoot-1.x/redfoot.py
+++ redfoot-1.x/redfoot.py
@@ -1,19 +1,16 @@
+import dev_hack
+
 import sys
+
 from redcmd import RedCmd
 
-class RedCmd(RedCmd): pass
-
-
-def error(arg):
-    print "No command for '%s'" % arg
-    
 if __name__ == "__main__":
     red_cmd = RedCmd()    
 
     argv = sys.argv
     if len(argv)==2:
         file = open(argv[1], "r")
-    for line in file:
-        red_cmd.cmdqueue.append(line)
+        for line in file:
+            red_cmd.cmdqueue.append(line)
 
     red_cmd.cmdloop()
