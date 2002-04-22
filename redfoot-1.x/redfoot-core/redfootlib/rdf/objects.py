6b252b8b7ce6630b30c34773f6a272f7e41f0ddc
Daniel Krech <eikeon@eikeon.com>
2002-04-22 06:20:56 +0000

    not sure we need to intern the objects anymore... more testing / thought required but checking in now

--- redfoot-1.x/redfoot-core/redfootlib/rdf/objects.py
+++ redfoot-1.x/redfoot-core/redfootlib/rdf/objects.py
@@ -1,33 +1,18 @@
 
-resources_dict = {}
-literal_dict = {}
-
 def resource(uri, anonymous=None):
-    if uri:
-        uri = intern(uri)
     if uri==None:
-        r = None
-    elif resources_dict.has_key(uri):
-        r = resources_dict[uri]
+        return None
+    elif anonymous:
+        r = AnonymousResource(uri)
     else:
-        if anonymous:
-            r = resources_dict[uri] = AnonymousResource(uri)
-        else:
-            r = resources_dict[uri] = Resource(uri)
-
+        r = Resource(uri)
     return r
 
 def literal(value):
-    if value:
-        value = intern(value)
     if value==None:
-        r = None
-    elif literal_dict.has_key(value):
-        r = literal_dict[value]
+        return None
     else:
-        r = literal_dict[value] = Literal(value)
-    return r
-
+        return Literal(value)
 
 class AnonymousResource(str):
     def is_literal(self):
