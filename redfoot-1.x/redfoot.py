9a11b0588e5c0ea437f4fa8520ce88d5181b9d17
Daniel Krech <eikeon@eikeon.com>
2002-04-23 03:30:30 +0000

    a mixture of small changes

--- redfoot-1.x/redfoot.py
+++ redfoot-1.x/redfoot.py
@@ -11,30 +11,6 @@ USAGE: redfoot.py <redfoot command file>
     sys.exit(-1)
 
 
-if not hasattr(sys, 'version_info') or sys.version_info[0]<2:
-    print """\
-Can not run redfoot with Python verion:
-  '%s'""" % sys.version
-    print "Redfoot requires Python 2.0 or higher to run. "
-    sys.exit(-1)
-else:
-    if sys.version_info[1]<2:
-        print """\
-Warning: Redfoot not tested or known to run with Python version less than 2.2
-"""
-    elif sys.version_info[2]<1:
-        print """\
-Warning: Redfoot requires a bug fix from 2.2.1 in order to run correctly
-"""
-        
-
-try:
-    import threading
-except ImportError:
-    print """
-Redfoot can not run without the threading module. Check that your PYTHONPATH is right and that you have threading.py
-"""
-    sys.exit(-1)
 
 
 if __name__ == "__main__":
