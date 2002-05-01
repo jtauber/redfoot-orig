1bef9e64ff118db10de037a2006bf766846b4ca5
Daniel Krech <eikeon@eikeon.com>
2002-05-01 05:20:31 +0000

    first cutt

---
+++ redfoot-1.6/examples/rdf_app.py
@@ -0,0 +1,179 @@
+####
+from redfootlib.server.module import App
+from redfootlib.rdf.objects import resource, literal
+from redfootlib.rdf.const import TYPE, LABEL, COMMENT
+
+HTML = resource("http://redfoot.net/04/26/rapp/html")
+OUTER_HTML = resource("http://redfoot.net/04/26/rapp/outer_html")
+INNER_HTML = resource("http://redfoot.net/04/26/rapp/inner_html")
+ALIAS_TO = resource("http://redfoot.net/04/26/rapp/alias_to")
+CHAIN_TO = resource("http://redfoot.net/04/26/rapp/chain_to")
+
+from rdf_app_parser import parse
+
+class Processor(object):
+    
+    class State(object):
+        __slots__ = ('consumed', 'remaining', 'next', 'followed', 'outer')
+        def __init__(self, consumed='', remaining='', next=None):
+            self.consumed = consumed
+            self.remaining = remaining
+            self.next = next
+            self.followed = []
+            self.outer = []
+        def __repr__(self):
+            return "(%s, %s, %s, %s)" % (self.consumed, self.remaining, self.next, self.followed)
+    
+    def __init__(self, rednode):
+        self.rednode = rednode
+        self.__states = []
+
+    def get_state(self):
+        return self.__states[-1]
+
+    state = property(get_state)
+    
+    def handle_request(self, request, response):
+        self.request = request
+        self.response = response
+        path = self.rednode.uri + request.get_path_info()[1:]
+        initial = Processor.State('', path, self.exact)
+        self.__states = [initial, ]
+
+    def make_root(self):
+        print "STATES:", self.__states
+        self.__states = [self.__states[-1],]
+
+    def stop(self):
+        for check in self.state_iter():
+            self.__states.pop()
+    
+
+    def __next(self):
+        if self.__states:
+            state = self.__states[-1]
+            if state.next:
+                return state.next
+            else:
+                self.__states.pop()
+                return self.__next()
+        else:
+            return None
+        
+    def state_iter(self):
+        return iter(self.__next, None)
+
+    def exact(self):
+        state = self.__states[-1]
+        path = state.consumed + state.remaining
+        html = self.rednode.get_first_value(resource(path), HTML, None)
+        if html:
+            print "EXACT: %s" % path            
+            self.response.write(html)
+            self.__states.pop()
+        else:
+            state.next = self.alias
+
+    def alias(self):
+        state = self.__states[-1]
+        path = state.consumed + state.remaining        
+
+        class _Alias:
+            def __init__(self):
+                self.source = None
+                self.longest = 0
+            def __call__(self, source, p, destination):
+                if len(source)>=len(state.consumed):
+                    if path.find(source)==0:
+                        if len(source)>self.longest:
+                            self.longest = len(source)
+                            self.source = source
+                            self.destination = destination
+                            self.type = p
+        cb = _Alias()                            
+        self.rednode.visit(cb, (None, ALIAS_TO, None))
+        self.rednode.visit(cb, (None, CHAIN_TO, None))        
+
+        if cb.source and not cb.source in state.followed:
+            print "ALIAS: %s->%s" % (cb.source, cb.destination)            
+            state.followed.append(cb.source)
+            new_state = Processor.State()
+            new_state.consumed = cb.destination
+            new_state.remaining = path[len(cb.source):]
+            new_state.next = self.exact
+            new_state.followed = list(state.followed)
+            if cb.type==ALIAS_TO:
+                self.__states = [] # clear states
+            self.__states.append(new_state)
+        else:
+            state.next = self.outer
+
+    def outer(self):
+        state = self.__states[-1]
+        path = state.consumed + state.remaining
+        l = []
+        def _outer(subject, predicate, object):
+            l.append(subject)
+        self.rednode.visit(_outer, (None, OUTER_HTML, None))
+        
+        l.sort(lambda a, b: len(a)-len(b))
+        for o in l:
+            if len(o)>=len(state.consumed) and o.find(state.consumed)==0:
+                if path.find(o)==0 and not o in state.outer:
+                    state.outer.append(o)
+                    state.consumed = o
+                    state.remaining = path[len(o):]
+                    state.next = self.outer
+                    print "OUTER:", o
+                    data = self.rednode.get_first_value(o, OUTER_HTML, None)
+                    if data:
+                        #state.next = self.inner
+                        facet = self._parse(data, o)
+                        if facet:
+                            facet(self.rednode, self.request,
+                                  self.response, self)
+                            return
+                        
+        state.next = self.inner
+
+
+    def inner(self):
+        state = self.__states[-1]
+        path = state.consumed + state.remaining
+        html = self.rednode.get_first_value(resource(path), INNER_HTML, None)
+        if html:
+            print "INNER: %s" % path            
+            self.response.write(html)            
+        self.__states.pop()
+
+    def process(self):
+        for check in self.state_iter():
+            check()
+
+    def _parse(self, codestr, facet_uri):
+        locals = globals = {}
+        try:
+            parse(codestr, facet_uri, globals, locals)
+        except:
+            from traceback import print_exc
+            print_exc()                
+            def error(rednode, request, response, process):
+                response.write("""
+<h1>Error: Could not parse outer_html for '%s':</h1>
+<pre>%s</pre>""" % (facet_uri, codestr))
+            return error
+        return globals.get(facet_uri, None)
+
+
+class RDFApp(App):
+
+    def __init__(self, rednode):
+        super(RDFApp, self).__init__(rednode)
+    
+    def handle_request(self, request, response):
+        processor = Processor(self.rednode)
+        processor.handle_request(request, response)
+        processor.process()
+        response.close()
+
+    
---
+++ redfoot-1.6/examples/rdf_app_parser.py
@@ -0,0 +1,116 @@
+from xml.parsers.expat import ParserCreate
+
+from redfootlib.xml.handler import HandlerBase
+from redfootlib.rdf.objects import resource, literal
+
+from exceptions import SyntaxError, Exception
+
+APPLY = resource("http://redfoot.net/04/26/rapp/^apply")
+EXEC = resource("http://redfoot.net/04/26/rapp/^exec")
+
+class HTMLHandler(HandlerBase, object):
+
+    def __init__(self, parser):
+        HandlerBase.__init__(self, parser, None)
+        self.data = ''
+        args = "rednode, request, response, processor"
+        self.codestr = '''\
+def _tmp(%s):
+'''  % args
+
+    def child(self, name, atts):
+        if name==APPLY:
+            if self.data:
+                self.codestr = self.codestr + '''\
+        response.write("""%s""")
+''' % self.data
+                self.data = ''
+            self.codestr = self.codestr + '''\
+        processor.process()
+'''                
+        elif name==EXEC:
+            self.eh = Exec(self.parser, self)
+        else:
+            if len(atts)==0:
+                self.data = self.data + "<%s>" % name
+            else:
+                self.data = self.data + "<%s " % name
+                for item in atts.iteritems():
+                    self.data = self.data + '%s="%s"' % item
+                self.data = self.data +">"
+
+    def char(self, data):
+        self.data = self.data + data
+
+    def execstr(self, codestr):
+        if self.data:        
+            self.codestr = self.codestr + '''\
+        response.write("""%s""")
+''' % self.data
+            self.data = ''
+        for line in codestr.split("\n"):
+            self.codestr = self.codestr + '''\
+        %s
+''' % line        
+
+    def end(self, name):        
+        if name!=APPLY:
+            self.data = self.data + "</%s>" % name
+
+    def get_codestr(self):
+        if self.data:
+            self.codestr = self.codestr + '''\
+        response.write("""%s""")
+''' % self.data
+            self.data = ''
+        return self.codestr
+
+
+
+def parse(data, facet_uri, globals, locals):
+    parser = ParserCreate(namespace_separator="^")
+    parser.returns_unicode = 0
+    
+    htmlHandler = HTMLHandler(parser)
+    parser.Parse(data)
+
+    codestr = htmlHandler.get_codestr()
+    try:
+        exec codestr+"\n" in globals, locals
+        globals[facet_uri] = locals['_tmp']
+        del locals['_tmp']
+    except Exception, e:
+        from traceback import print_exc
+        print_exc()                
+        print e, codestr
+    return
+
+
+from redfootlib.server.redcode.handlers import adjust_indent
+
+
+class Exec(HandlerBase, object):
+
+    def __init__(self, parser, parent):
+        HandlerBase.__init__(self, parser, parent,)
+        self.data = ""
+        
+    def child(self, name, atts):
+        if len(atts)==0:
+            self.data = self.data + "<%s>" % name
+        else:
+            self.data = self.data + "<%s " % name
+            for item in atts.iteritems():
+                self.data = self.data + '%s="%s"' % item
+            self.data = self.data +">"
+        
+    
+    def char(self, data):
+        self.data = self.data + data
+
+    def end(self, name):
+        if name==EXEC:
+            super(Exec, self).end(name)        
+            self.parent.execstr(adjust_indent(self.data))
+        else:
+            self.data = self.data + "</%s>" % name
