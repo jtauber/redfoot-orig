63a838fdb7d3d9266a68cbb181b7f31e2b069c3b
Daniel Krech <eikeon@eikeon.com>
2002-05-15 18:05:57 +0000

    a neighbourhood now has the uri of is local store

--- redfoot-1.6/lib/redfootlib/rdf/store/neighbourhood.py
+++ redfoot-1.6/lib/redfootlib/rdf/store/neighbourhood.py
@@ -5,6 +5,10 @@ class Neighbourhood(object):
         self.local = local
         self.neighbours = neighbours
 
+    def __get_uri(self):
+        return self.local.uri
+    uri = property(__get_uri)
+
     def visit(self, callback, triple):
         stop = self.local.visit(callback, triple)
         if stop:
