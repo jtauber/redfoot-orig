eb26ded859e6c67b847e88029b9f4597d536d39e
Daniel Krech <eikeon@eikeon.com>
2002-05-23 05:44:09 +0000

    split out visit methods into Visit mixin

--- redfoot-1.6/lib/redfootlib/rdf/query/core.py
+++ redfoot-1.6/lib/redfootlib/rdf/query/core.py
@@ -1,50 +1,22 @@
-
-from redfootlib.rdf.query.functors import *
-from redfootlib.rdf.query.builders import *
-
 from redfootlib.rdf.query.visit import Visit
 
 
 class Query(Visit):
     
     def exists(self, subject, predicate, object):
-        b = ItemBuilder()
-        self.visit(first(triple2statement(b.accept)), (subject, predicate, object))
-        return (b.item != None)
-
-    def remove(self, subject=None, predicate=None, object=None):
-        for s, p, o in self.triples(subject, predicate, object):
-            super(Query, self).remove(s, p, o)
+        for triple in self.triples(subject, predicate, object):
+            return 1
+        return 0
 
     def not_exists(self, subject, predicate, object):
+        """ TODO: seems like this method is a bit too
+        convenient... should # it go away? """
         return not self.exists(subject, predicate, object)
 
-    def get_first(self, subject, predicate, object):
-        b = ItemBuilder()
-        self.visit(first(triple2statement(b.accept)), (subject, predicate, object))
-        return b.item
-
     def get_first_value(self, subject, predicate, default=None):
-        s = self.get_first(subject, predicate, None)
-        if s:
-            return s.object
-        else:
-            return default
-
-    # TODO: currently goes into infinite loop if property loops back
-    def visit_transitive(self, callback, root, property):
-        self.visit(both(callback, o(callback_subject(self.visit_transitive, callback, property))),
-                   (root, property, None))
-
-    # TODO: currently goes into infinite loop if property loops back
-    def visit_transitive_reverse(self, callback, root, property):
-        self.visit(both(callback, s(callback_subject(self.visit_transitive_reverse, callback, property))),
-                   (None, property, root))
+        for (s, p, o) in self.triples(subject, predicate, None):
+            return o
+        return default
 
 
-    # TODO: not really a query
-    def generate_uri(self):
-        from redfootlib.rdf.store.urigen import generate_uri
-        from redfootlib.rdf.objects import resource
-        return resource(self.uri + generate_uri())
 
--- redfoot-1.6/lib/redfootlib/rdf/query/schema.py
+++ redfoot-1.6/lib/redfootlib/rdf/query/schema.py
@@ -9,136 +9,27 @@ from redfootlib.rdf.const import *
 class SchemaQuery(Query):
 
     # TODO: should we have a version of this that answers for subclasses too?
-    def is_of_type(self, res, type):
-        return self.exists(res, TYPE, type)
+    def is_of_type(self, subject, type):
+        return self.exists(subject, TYPE, type)
 
-    def visit_typeless_resources(self, callback):
-        self.visit_subjects(filter(s(callback), s(self.not_exists, TYPE, None)))
-
-    def visit_by_type(self, callback, type, predicate, object):
-        self.visit(filter(callback, s(self.is_of_type, type)), (None, predicate, object))
-
-    # TODO: method to return all labels
     def label(self, subject, default=None):
-        # TODO: could be rewritten to use get_first_value
-        b = ItemBuilder()
-        self.visit(first(o(b.accept)), (subject, LABEL, None))
-        if b.item == None:
-            first_label = None
-        else:
-            first_label = b.item
-        if first_label:
-            return first_label
-        elif default != None:
-            return default
-        else:
-            return subject
+        for s, p, o in self.triples(subject, LABEL, None):
+            return o
+        return default or subject
 
     def comment(self, subject, default=None):
-        # TODO: could be rewritten to use get_first_value
-        b = ItemBuilder()
-        self.visit(first(o(b.accept)), (subject, COMMENT, None))
-        if b.item == None:
-            first_comment = None
-        else:
-            first_comment = b.item
-        if first_comment:
-            return first_comment
-        elif default != None:
-            return default
-        else:
-            return self.label(subject)
-
-    def visit_root_classes(self, callback):
-        self.visit(filter(callback, s(self.not_exists, SUBCLASSOF, None)), (None, TYPE, CLASS))
-
-    def visit_parent_types(self, callback, type):
-        self.visit(callback, (type, SUBCLASSOF, None))
-
-    def visit_possible_properties(self, callback, type):
-        def f(superType, callback=callback, self=self):
-            self.visit(s(callback), (None, DOMAIN, superType))
-        self.visit_transitive(o(f), type, SUBCLASSOF)
-        f(type)
-
-    def visit_possible_properties_for_subject(self, callback, res):
-        def f(type, callback=callback, self=self):
-            self.visit_possible_properties(callback, type)
-        self.visit(o(f), (res, TYPE, None))
-
-    def visit_ranges(self, callback, property):
-        self.visit(o(callback), (property, RANGE, None))
-
-    # callback may be called more than once for the same possibleValue...
-    # userof this method will have to remove duplicates
-    def visit_possible_values(self, callback, property):
-        def f(type, callback=callback, self=self):
-            def e(type, callback=callback, self=self):
-                self.visit(s(callback), (None, TYPE, type))
-            self.visit_transitive_reverse(s(e), type, SUBCLASSOF)
-            e(type)
-        self.visit_ranges(f, property)
-
-    def visit_resources_by_type(self, class_callback, instance_callback):
-        b = SetBuilder()
-        self.visit(o(b.accept), (None, TYPE, None))
-        for type in b.set:
-            if self.exists(None, TYPE, type):
-                class_callback(type)
-                self.visit(s(instance_callback), (None, TYPE, type))
-
-    def visit_subclasses(self, class_start_callback, class_end_callback, instance_callback, root,
-                         recurse=1, depth=0):
-        class_start_callback(root, depth)
-
-        def f(type,
-              self=self, class_start_callback=class_start_callback,
-              class_end_callback=class_end_callback, instance_callback=instance_callback, depth=depth):
-            self.visit_subclasses(class_start_callback, class_end_callback, instance_callback,
-                                  type, 1, depth + 1)
-
-        def g(instance,
-              depth=depth, instance_callback=instance_callback):
-            instance_callback(instance, depth)
-
-        def h(klass,
-              depth=depth, class_start_callback=class_start_callback, class_end_callback=class_end_callback):
-            class_start_callback(klass, depth + 1)
-            class_end_callback(klass, depth + 1)
-
-        if recurse:
-            self.visit(s(f), (None, SUBCLASSOF, root))
-        else:
-            self.visit(s(h), (None, SUBCLASSOF, root))
-
-        self.visit(s(g), (None, TYPE, root))
-
-        class_end_callback(root, depth)
-
-    # REIFICATION
-    def visit_reified_statements_about_subject(self, callback, subject):
-        def f(statement_uri, subject=subject, self=self, callback=callback):
-            callback(statement_uri, subject, self.get_first(statement_uri, PREDICATE, None).object, self.get_first(statement_uri, OBJECT, None).object)
-        self.visit_by_type(s(f), STATEMENT, SUBJECT, subject)
-
-
+        for s, p, o in self.triples(subject, COMMENT, None):
+            return o
+        return default or self.label(subject)
+        
     def get_statement_uri(self, subject, predicate, object):
         """\
         Returns the first statement uri for the given subject, predicate, object.
         """
-        class Result:
-            def __init__(self, value):
-                self.value = value
-        result = Result(None)
-        def _callback(s, p, o):
-            if not self.exists(s, SUBJECT, subject):
-                return
-            if not self.exists(s, PREDICATE, predicate):
-                return
-            if not self.exists(s, OBJECT, object):
-                return
-            result.value = s
-            return 1
-        self.visit(_callback, (None, TYPE, STATEMENT))
-        return result.value
+        for (s, p, o) in self.triples(None, TYPE, STATEMENT):
+            if self.exists(s, SUBJECT, subject)\
+            and self.exists(s, PREDICATE, predicate)\
+            and self.exists(s, OBJECT, object):
+                return s
+        return None
 
