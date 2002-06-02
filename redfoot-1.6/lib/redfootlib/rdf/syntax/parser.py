6c21f3a0d1ba507fd95198796c673b9f2f2f6d89
Daniel Krech <eikeon@eikeon.com>
2002-06-02 22:00:34 +0000

    rewrite of current functionality

--- redfoot-1.6/lib/redfootlib/rdf/syntax/parser.py
+++ redfoot-1.6/lib/redfootlib/rdf/syntax/parser.py
@@ -1,39 +1,6 @@
-
-from redfootlib.xml.handler import HandlerBase as AbstractHandlerBase
-
-class HandlerBase(AbstractHandlerBase):
-
-    def __init__(self, parser, parent, adder):
-        AbstractHandlerBase.__init__(self, parser, parent)
-        self.adder = adder
-
-from redfootlib.rdf.store.urigen import generate_uri
-
-
-ANON = "http://redfoot.sourceforge.net/2001/08/ANON/"
-
-#TODO flag for where RDF can be assumed
-#TODO proper handling of relative URIs
-
 ns_separator = ""
 
-def parse(adder, file, baseURI):
-    import xml.parsers.expat
-
-    parser = xml.parsers.expat.ParserCreate(namespace_separator=ns_separator)
-    parser.SetBase(baseURI)
-    parser.returns_unicode = 0
-    
-    LookForRDFHandler(parser, None, adder)
-
-    parser.ParseFile(file)
-        
-    file.close()
-
-    
-# TODO: do we still want the level of isolation where the parser /
-# serializer do not know about the objects? If so, we will have to
-# change this... as TYPE is now an object.
+from redfootlib.rdf.objects import URIRef, Literal, BNode
 from redfootlib.rdf.const import RDFNS, TYPE
 
 RDF_ELEMENT = RDFNS + ns_separator + "RDF"
@@ -47,236 +14,243 @@ BAG_ELEMENT = RDFNS + ns_separator + "Bag"
 ALT_ELEMENT = RDFNS + ns_separator + "Alt"
 LI_ELEMENT = RDFNS + ns_separator + "li"
 
-
-class LookForRDFHandler(HandlerBase):
-    def __init__(self, parser, parent, adder):
-        HandlerBase.__init__(self, parser, parent, adder)
-
-    def child(self, name, atts):
-        if name == RDF_ELEMENT:
-            RDFHandler(self.parser, self, self.adder)
-        else:
-            LookForRDFHandler(self.parser, self, self.adder)
-
-
-class RDFHandler(HandlerBase):
-    def __init__(self, parser, parent, adder):
-        HandlerBase.__init__(self, parser, parent, adder)
-
-    def child(self, name, atts):
-        if name == DESCRIPTION_ELEMENT:
-            DescriptionHandler(self.parser, self, self.adder, atts)
-        elif name == BAG_ELEMENT:
-            BagHandler(self.parser, self, self.adder, name, atts)
-        elif name == ALT_ELEMENT:
-            AltHandler(self.parser, self, self.adder, name, atts)
-        elif name == SEQ_ELEMENT:
-            SeqHandler(self.parser, self, self.adder, name, atts)
-        else:
-            TypedNodeHandler(self.parser, self, self.adder, name, atts)
-
-
-def absolutize(base, uri):
-    if uri == '' or uri[0] == "#":
-        return base + uri
-    else:
-        return uri
-
-
-
-class DescriptionHandler(HandlerBase):
-    def __init__(self, parser, parent, adder, atts):
-        HandlerBase.__init__(self, parser, parent, adder)
-        self.subject = None
-        self.anonymous = 0
-        if atts.has_key("about"):
-            self.subject = atts["about"]
-        elif atts.has_key("ID"):
-            self.subject = "#" + atts["ID"]
-        elif atts.has_key(ABOUT_ATTRIBUTE):
-            self.subject = atts[ABOUT_ATTRIBUTE]
-        elif atts.has_key(ID_ATTRIBUTE):
-            self.subject = "#" + atts[ID_ATTRIBUTE]
-        else:
-            self.subject = ANON + generate_uri()
-            self.anonymous = 1
-
-        self.subject = absolutize(self.parser.GetBase(), self.subject)
-
-        for att in atts.keys():
-            if att == "about" or att == "ID" or \
-               att == ABOUT_ATTRIBUTE or att == ID_ATTRIBUTE:
-                pass
-            else:
-                self.adder(self.subject, att, atts[att],
-                           anonymous_subject=self.anonymous, literal_object=1)
-
-    def child(self, name, atts):
-        PropertyHandler(self.parser, self, self.adder, name, atts)
-                        
-
-class TypedNodeHandler(DescriptionHandler):
-    def __init__(self, parser, parent, adder, name, atts):
-        DescriptionHandler.__init__(self, parser, parent, adder, atts)
-        type = name
-        adder(self.subject, str(TYPE), type, anonymous_subject=self.anonymous)
-
-
 def all_whitespace(data):
     for char in data:
         if char not in [chr(9),chr(10),chr(13),chr(32)]:
             return 0
     return 1
 
+class DocumentHandler(object):
+    def __init__(self, parser, add):
+        self.parser = parser
+        self.add = add
+        self.base = parser.GetBase()
+        parser.StartElementHandler = self.child
+        parser.CharacterDataHandler = self.char
+        parser.EndElementHandler = self.end
+        self.child_stack = [self.look_child,]
+        self.char_stack = [self.default_char,]
+        self.end_stack = [self.look_end,]
 
-class ContainerHandler(TypedNodeHandler):
-    def __init__(self, parser, parent, adder, name, atts):
-        TypedNodeHandler.__init__(self, parser, parent, adder, name, atts)
-        parent.object = self.subject
-        parent.anonymous_object = self.anonymous
-        self.li_count = 0
+    def absolutize(self, uri):
+        if uri == '' or uri[0] == "#":
+            return self.base + uri
+        else:
+            return uri
 
     def child(self, name, atts):
-        if name == LI_ELEMENT:
-            LIHandler(self.parser, self, self.adder, atts)
-        else:
-            PropertyHandler(self.parser, self, self.adder, name, atts)
+        self.child_stack[-1](name, atts)        
 
-    def add_li(self, value, literal):
-        self.li_count = self.li_count + 1
-        predicate = RDFNS + "_" + str(self.li_count)
-        self.adder(self.subject, predicate, value, literal_object=literal, anonymous_subject=self.anonymous)
-
-
-class BagHandler(ContainerHandler):
-    pass
-
-
-class AltHandler(ContainerHandler):
-    pass
-
-
-class SeqHandler(ContainerHandler):
-    pass
-
-
-class LIHandler(HandlerBase):
-    def __init__(self, parser, parent, adder, atts):
-        HandlerBase.__init__(self, parser, parent, adder)
-        self.literal = 0
-        if atts.has_key("resource"):
-            self.value = atts["resource"]
-            self.value = absolutize(self.parser.GetBase(), self.value)                
-        elif atts.has_key(RESOURCE_ATTRIBUTE):
-            self.value = atts[RESOURCE_ATTRIBUTE]
-            self.value = absolutize(self.parser.GetBase(), self.value)
-        else:
-            self.value = ""
-            self.literal = 1
-            
     def char(self, data):
-        if not all_whitespace(data):
-            if not self.literal:
-                raise "has both character content and a resource attribute"
-            self.value = self.value + data
-
+        self.char_stack[-1](data)
+    
     def end(self, name):
-        self.parent.add_li(self.value, self.literal)
-        HandlerBase.end(self, name)
+        self.end_stack[-1](name)
+
+    def look_child(self, name, atts):
+        if name == RDF_ELEMENT:
+            self.child_stack.append(self.rdf_child)            
+
+    def look_end(self, name):
+        pass # Keep looking
+
+    def default_child(self, name, atts):
+        self.child_stack.append(self.default_child)
+        self.end_stack.append(self.default_end)        
+
+    def default_char(self, data):
+        pass
+
+    def default_end(self, name):
+        self.child_stack.pop()
+        self.end_stack.pop()
+
+    def rdf_child(self, name, atts):
+        if name == DESCRIPTION_ELEMENT:
+            self.description(name, atts)            
+            self.child_stack.append(self.description_child)
+            self.end_stack.append(self.default_end)                
+        elif name == BAG_ELEMENT:
+            self.bag(name, atts)                        
+            self.child_stack.append(self.container_child)
+            self.end_stack.append(self.default_end)
+        elif name == ALT_ELEMENT:
+            self.alt(name, atts)                        
+            self.child_stack.append(self.container_child)
+            self.end_stack.append(self.default_end)
+        elif name == SEQ_ELEMENT:
+            self.sequence(name, atts)            
+            self.child_stack.append(self.container_child)
+            self.end_stack.append(self.default_end)
+        else:
+            self.typed_node(name, atts)            
+            self.child_stack.append(self.description_child)
+            self.end_stack.append(self.default_end)                
 
 
-class PropertyHandler(HandlerBase):
-    def __init__(self, parser, parent, adder, name, atts):
-        HandlerBase.__init__(self, parser, parent, adder)
+    def typed_node(self, name, atts):
+        self.subject = None
+        if atts.has_key("about"):
+            self.subject = URIRef(self.absolutize(atts["about"]))
+        elif atts.has_key("ID"):
+            self.subject = URIRef(self.base + "#" + atts["ID"])
+        elif atts.has_key(ABOUT_ATTRIBUTE):
+            self.subject = URIRef(self.absolutize(atts[ABOUT_ATTRIBUTE]))
+        elif atts.has_key(ID_ATTRIBUTE):
+            self.subject = URIRef(self.base + "#" + atts[ID_ATTRIBUTE])
+        else:
+            self.subject = BNode()
+
+        for att in atts.keys():
+            if att == "about" or att == "ID" or \
+                   att == ABOUT_ATTRIBUTE or att == ID_ATTRIBUTE:
+                pass
+            else:
+                self.add(self.subject, att, Literal(atts[att]))
+
+    def description(self, name, atts):
+        self.typed_node(name, atts)
+        self.add(self.subject, str(TYPE), URIRef(name))
+
+    def description_child(self, name, atts):
+        self.property(name, atts)        
+        self.child_stack.append(self.property_child)
+        self.char_stack.append(self.property_char)
+        self.end_stack.append(self.property_end)                        
+
+
+    def property(self, name, atts):
         self.predicate = name
-        self.literal = 0
-        self.anonymous_object = 0
         if atts.has_key("resource"):
-            self.object = atts["resource"]
-            self.object = absolutize(self.parser.GetBase(), self.object)                                                
+            self.object = self.absolutize(atts["resource"])
             for att in atts.keys():
                 if att == "resource":
                     pass
                 else:
-                    self.adder(self.object, att, atts[att],
-                               literal_object=1)
+                    self.add(self.object, att, Literal(atts[att]))
         elif atts.has_key(RESOURCE_ATTRIBUTE):
-            self.object = atts[RESOURCE_ATTRIBUTE]
-            self.object = absolutize(self.parser.GetBase(), self.object)
+            self.object = URIRef(self.absolutize(atts[RESOURCE_ATTRIBUTE]))
             for att in atts.keys():
                 if att == RESOURCE_ATTRIBUTE:
                     pass
                 else:
-                    self.adder(self.object, att,atts[att],
-                               literal_object=1)
+                    self.add(self.object, att, Literal(atts[att]))
         else:
-            self.object = ""
-            self.literal = 1
-
-    def child(self, name, atts):
-        self.literal = 0
+            self.object = Literal("")
+        
+    def property_child(self, name, atts):
         if name==SEQ_ELEMENT:
-            SeqHandler(self.parser, self, self.adder, name, atts)
+            self.sequence(name, atts)                        
+            self.child_stack.append(self.container_child)
+            self.end_stack.append(self.default_end)
         elif name==BAG_ELEMENT:
-            BagHandler(self.parser, self, self.adder, name, atts)
+            self.bag(name, atts)                                    
+            self.child_stack.append(self.container_child)
+            self.end_stack.append(self.default_end)
         elif name==ALT_ELEMENT:
-            AltHandler(self.parser, self, self.adder, name, atts)
+            self.alt(name, atts)                                    
+            self.child_stack.append(self.container_child)
+            self.end_stack.append(self.default_end)
         else:
             if atts.has_key("about"):
-                self.object = atts["about"]
+                self.object = URIRef(self.absolutize(atts["about"]))
             elif atts.has_key("ID"):
-                self.object = self.parser.GetBase() + "#" + atts["ID"]
+                self.object = URIRef(self.uri + "#" + atts["ID"])
             elif atts.has_key(ABOUT_ATTRIBUTE):
-                self.object = atts[ABOUT_ATTRIBUTE]
+                self.object = URIRef(self.absolutize(atts[ABOUT_ATTRIBUTE]))
             elif atts.has_key(ID_ATTRIBUTE):
-                self.object = self.parser.GetBase() + "#" + atts[ID_ATTRIBUTE]
+                self.object = URIRef(self.uri + "#" + atts[ID_ATTRIBUTE])
             else:
                 print "Descriptions must have either an about or an ID '%s'" % name
             if name==DESCRIPTION_ELEMENT:
-                DescriptionHandler(self.parser, self, self.adder, atts)
+                self.description(name, atts)                
+                self.child_stack.append(self.description_child)
+                self.end_stack.append(self.default_end)                
             else:
-                TypedNodeHandler(self.parser, self, self.adder, name, atts)
+                self.typed_node(name, atts)                
+                self.child_stack.append(self.description_child)
+                self.end_stack.append(self.default_end)                
 
-    def char(self, data):
+    def property_char(self, data):
         if not all_whitespace(data):
-            if not self.literal:
+            if not isinstance(self.object, Literal):
                 raise "has both character content and a resource attribute"
             self.object = self.object + data
+
+    def property_end(self, name):
+        self.add(self.subject, self.predicate, self.object)
+        self.child_stack.pop()
+        self.char_stack.pop()
+        self.end_stack.pop()
+
+    def container(self, name, atts):
+        self.typed_node(name, atts)
+        self.object = self.subject
+        self.li_count = 0
+
+    def container_child(self, name, atts):
+        if name == LI_ELEMENT:
+            self.li(name, atts)            
+            self.child_stack.append(self.li_child)
+            self.char_stack.append(self.li_char)
+            self.end_stack.append(self.li_end)
         else:
-            if self.literal:
-                self.object = self.object + data
+            self.property(name, atts)            
+            self.child_stack.append(self.property_child)
+            self.char_stack.append(self.property_char)
+            self.end_stack.append(self.property_end)                        
 
-    def end(self, name):
-        self.adder(self.parent.subject, self.predicate, self.object,
-                   literal_object=self.literal, anonymous_object=self.anonymous_object)
-        HandlerBase.end(self, name)
+    def bag(self, name, atts):
+        self.container(name, atts)
 
+    def alt(self, name, atts):
+        self.container(name, atts)
 
+    def sequence(self, name, atts):
+        self.container(name, atts)
 
+    def li(self, name, atts):
+        if atts.has_key("resource"):
+            self.value = URIRef(self.absolutize(atts["resource"]))
+        elif atts.has_key(RESOURCE_ATTRIBUTE):
+            self.value = URIRef(self.uri + atts[RESOURCE_ATTRIBUTE])
+        else:
+            self.value = Literal("")
 
-from redfootlib.rdf.objects import resource, literal
+    def li_child(self, name, atts):
+        raise "Not allowed"
+
+    def li_char(self, data):
+        if not all_whitespace(data):
+            if not isinstance(self.value, Literal):
+                raise "has both character content and a resource attribute"
+            self.value = self.value + data
+
+    def li_end(self, name):
+        self.li_count = self.li_count + 1
+        predicate = RDFNS + "_" + str(self.li_count)
+        self.add(self.subject, predicate, self.value)
+        self.child_stack.pop()
+        self.char_stack.pop()
+        self.end_stack.pop()
+
 
 class Parser(object):
     def __init__(self):
         super(Parser, self).__init__()
 
-    def add_statement(self, subject, predicate, object, literal_object=None, anonymous_subject=None, anonymous_object=None):
-        s = resource(subject, anonymous_subject)
-        p = resource(predicate)
-        if literal_object:
-            o = literal(object)
-        else:
-            o = resource(object, anonymous_object)
-        self.add(s, p, o)
-
     def parse(self, file, baseURI):
-        parse(self.add_statement, file, baseURI)
+        from xml.parsers.expat import ParserCreate        
+        parser = ParserCreate(namespace_separator=ns_separator)
+        parser.SetBase(baseURI)
+        parser.returns_unicode = 0
+        DocumentHandler(parser, self.add)
+        parser.ParseFile(file)
+        file.close()
 
     def parse_URI(self, location, baseURI=None):
         baseURI = baseURI or location
-        
         from urllib import urlopen
         file = urlopen(location)
-        
         self.parse(file, baseURI)
         
